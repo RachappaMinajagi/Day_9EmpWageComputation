54ec076a55e437069a235d98424413fd82927aef
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-23 15:47:57 +0530

    PartTime Employee and FullTime Employee Wage

--- src/empWage/EmpWage.java
+++ src/empWage/EmpWage.java
@@ -2,42 +2,27 @@ package empWage;
 
 import java.util.Random; //import Random function
 
-class utilityEmpWage {
-	Random rand = new Random();
-    public int isPresent = 1;
-    public int isAbsent = 2;
-    public int isPartTime = 3;
-    public int wagePerHr = 20;
-    public int fullDayHr = 8;
-    public int partTimeHr = 4;
-    public int dailyWage ;
-
-
-    public void present() {
-        int check = rand.nextInt(3)+1;
-        if (check == isAbsent) {
-            System.out.println("Employee is absent.");
-
-        }
-        else if (check == isPresent) {
-            System.out.println("Employee is present.");
-            dailyWage = wagePerHr*fullDayHr;
-            System.out.println("Wage of the day is : " + dailyWage) ;
-        }
-        else {
-            System.out.println("Employee is present part time .");
-            dailyWage = wagePerHr*partTimeHr;
-            System.out.println("Wage of the day is " + dailyWage);
-        }
-    }
-}
-
-public class EmpWage {
+public class EmpWageComputation {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation");
-		utilityEmpWage empWage = new utilityEmpWage();
-		empWage.present();
-	}
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int Wage_HR = 20;
+		int Day_HRs = 8;
+		int Part_HRs = 4;
 
+		Random random = new Random();
+		int r = random.nextInt(3);
+		if (r == 1) {
+			// Employee Present or Absent and Assume Wage per hsr per day hrs part time and
+			// full time employee
+			System.out.println("Employee Present");
+			System.out.println("DayWage = " + Wage_HR * Day_HRs);
+		} else if (r == 2) {
+			System.out.println("Employee Present");
+			System.out.println("DayWage with Part Time = " + Wage_HR *Part_HRs); // part time Employee
+		}
+
+		else
+			System.out.println("Employee Absent");
+	}
 }

0658061273c8e2c8a4c5c13aecf9e35c191dc287
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-23 15:16:28 +0530

    Slove the Switchcase statement

--- src/empWage/EmpWage.java
+++ src/empWage/EmpWage.java
@@ -1,43 +1,38 @@
 package empWage;
 
-import java.util.Random; //import Random function
+import java.util.Random;
 
-class utilityEmpWage {
-	Random rand = new Random();
-    public int isPresent = 1;
-    public int isAbsent = 2;
-    public int isPartTime = 3;
-    public int wagePerHr = 20;
-    public int fullDayHr = 8;
-    public int partTimeHr = 4;
-    public int dailyWage ;
-
-
-    public void present() {
-        int check = rand.nextInt(3)+1;
-        if (check == isAbsent) {
-            System.out.println("Employee is absent.");
-
-        }
-        else if (check == isPresent) {
-            System.out.println("Employee is present.");
-            dailyWage = wagePerHr*fullDayHr;
-            System.out.println("Wage of the day is : " + dailyWage) ;
-        }
-        else {
-            System.out.println("Employee is present part time .");
-            dailyWage = wagePerHr*partTimeHr;
-            System.out.println("Wage of the day is " + dailyWage);
-        }
-    }
-}
 
 public class EmpWage {
+    public static void EmpWageStatus(int num){
+        int Wage_HR = 20;
+        int Day_HRs = 8;
+        int Part_HRs = 4;
+/*
+ * Switch case
+ */
+        switch (num){
+            case 1:
+                System.out.println("Employee Present");
+                System.out.println("DayWage = " + Wage_HR * Day_HRs);
+                break;
+            case 2:
+                System.out.println("Employee Present");
+                System.out.println("DayWage with Part Time = " + Wage_HR *Part_HRs);
+                break;
+            case 0:
+                System.out.println("Employee Absent");
+                System.out.println("DayWage = "+0);
+        }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation");
-		utilityEmpWage empWage = new utilityEmpWage();
-		empWage.present();
-	}
 
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+        Random random = new Random();
+        int r = random.nextInt(3);
+        EmpWageStatus(r);
+
+    }
 }

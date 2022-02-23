2228932a991b39676a9fa8ed4fdd12baeefcdfae
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-23 15:20:27 +0530

    Calculate the Wage for month

--- src/empWage/EmpWage.java
+++ src/empWage/EmpWage.java
@@ -1,38 +1,40 @@
 package empWage;
-
 import java.util.Random;
-
-
-public class EmpWage {
-    public static void EmpWageStatus(int num){
-        int Wage_HR = 20;
-        int Day_HRs = 8;
-        int Part_HRs = 4;
 /*
- * Switch case
+ * USing SwitchCase Wages for month 
  */
-        switch (num){
-            case 1:
-                System.out.println("Employee Present");
-                System.out.println("DayWage = " + Wage_HR * Day_HRs);
-                break;
-            case 2:
-                System.out.println("Employee Present");
-                System.out.println("DayWage with Part Time = " + Wage_HR *Part_HRs);
-                break;
-            case 0:
-                System.out.println("Employee Absent");
-                System.out.println("DayWage = "+0);
-        }
+public class EmpWage {
+	public static void EmpWageStatus(int num) {
+		int Wage_HR = 20;
+		int Day_HRs = 8;
+		int Part_HRs = 8;
+		int NumDays = 20;
+		double daywage = 0;
+		double monthwage = 0;
 
+		for (int i = 0; i < NumDays; i++) {
+			switch (num) {
+			case 1:
+				daywage = Wage_HR * Day_HRs * i;
+				break;
+			case 2:
+				daywage = Wage_HR * (Day_HRs + Part_HRs) * i;
+				break;
+			case 0:
+				break;
 
-    }
+			}
+			monthwage = monthwage + daywage;
+		}
+		System.out.println("Month Wage : " + monthwage);
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program");
-        Random random = new Random();
-        int r = random.nextInt(3);
-        EmpWageStatus(r);
+	}
 
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		Random random = new Random();
+		int r = random.nextInt(3);
+		EmpWageStatus(r);
+
+	}
 }
